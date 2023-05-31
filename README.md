53f33e76f38bdb7f723d3f76ce0941ecefb5e3aa
dimdimah <dimahluthfi15@gmail.com>
2023-05-31 22:08:18 +0700

    commitan pertama

---
+++ README.md
@@ -0,0 +1,44 @@
+## KELOMPOK 3 JAVA PROJEK
+
+Leader `M. Luqmanul Hakim`
+Coder `D. Luthfi Aulia Rohman`
+Tester `Hadyan Syaputra`
+Designer` M. Hasyim Abdullah`
+
+## DOKUMENTASI JAVA PROJEK
+
+DESKRIPSI PROGRAM
+
+Program Mencari Diskon adalah sebuah program sederhana yang dibuat menggunakan bahasa pemrograman Java. Program ini memungkinkan pengguna/user untuk mencari harga setelah dan sebelum diskon.
+
+RUMUS PROGRAM
+
+- Rumus mencari Harga sebelum Diskon
+
+  `Harga Sebelum Diskon = Harga Setelah Diskon / (1 - Persentase Diskon / 100)`
+
+  Dalam rumus di atas, Harga Awal adalah harga barang sebelum diskon, dan Persentase Diskon adalah persentase diskon yang diinputkan.
+
+- Rumus mencari Harga setelah Diskon
+
+  `Harga Setelah Diskon = Harga Awal - (Harga Awal * Persentase Diskon / 100)`
+
+  Dalam rumus di atas, Harga Setelah Diskon adalah harga barang setelah diskon, dan Persentase Diskon adalah persentase diskon yang diinputkan.
+
+Anda dapat menggunakan rumus di atas untuk menghitung harga setelah diskon dan harga sebelum diskon dalam implementasi program Anda. Pastikan untuk mengganti variabel yang sesuai dengan nilai yang relevan dalam konteks Anda.
+
+CARA COMPILING PROGRAM
+
+- buka terminal pada vscode
+- masuk kedalam directory `src`
+- jalankan program dibawah:
+  ` javac namapackage/namaFile.java -d ../bin`
+- kemudian tekan `ENTER`
+
+CARA RUNING PROGRAM
+
+- buka terminal pada vscode
+- masuk kedalam directory `bin`
+- jalankan program dibawah:
+  ` java namaPackage.namaFile`
+- kemudian tekan `ENTER`
---
+++ src/Diskon/getDiskon.java
@@ -0,0 +1,53 @@
+package Diskon;
+
+import java.util.Scanner;
+
+public class getDiskon {
+    public static float HitungHargaDiskon(float input1, float input2) {
+
+        float diskon = (input2 / 100) * input1;
+        float hargaSetelahDiskon = input1 - diskon;
+
+        System.out.println("Harga Barang Diskon " + hargaSetelahDiskon);
+        return hargaSetelahDiskon;
+    }
+
+    public static float HitungHargaAwal(float input1, float input2) {
+        float hargaAwal = 100 / (100 - input2) * input1;
+        float hargaDiskon = hargaAwal - input1;
+
+        System.out.println("Harga Barang Diskon " + hargaAwal);
+        return hargaDiskon;
+    }
+
+    public static void main(String[] args) {
+        Scanner inputAngka;
+        int pilihan;
+        float harga, persen;
+        inputAngka = new Scanner(System.in);
+        System.out.println("Menghitung Harga Diskon");
+        System.out.println("1. mencari harga sebelum Diskon");
+        System.out.println("2. mencari harga sebelum Diskon");
+        System.out.println("=================================");
+
+        System.out.print("Masukan pilihan: ");
+        pilihan = inputAngka.nextInt();
+
+        System.out.print("Masukkan harga barang: ");
+        harga = inputAngka.nextFloat();
+
+        System.out.print("Masukkan persentase diskon: ");
+        persen = inputAngka.nextFloat();
+        System.out.println("=================================");
+        // Menampilkan hasil
+
+        if (pilihan == 1) {
+            HitungHargaDiskon(harga, persen);
+        } else if (pilihan == 2) {
+            HitungHargaAwal(harga, persen);
+        } else {
+            System.out.println("Operator tidak tersedia");
+        }
+        inputAngka.close();
+    }
+}
