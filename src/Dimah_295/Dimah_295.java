f3c4cb573b6dd5f14e2349df194bf1fcfb34f987
dimdimah <dimahluthfi15@gmail.com>
2023-06-06 23:15:36 +0700

    Tugas individu Dimah

---
+++ src/Dimah_295/Dimah_295.java
@@ -0,0 +1,9 @@
+package Dimah_295;
+
+public class Dimah_295 {
+    public static void main(String[] args) {
+        int myNim = 2113010295;
+        String myName = "Dimah Luthfi Aulia Rohman";
+        System.out.println("Hallo nama saya " + myName + " dengan Nomer Induk Mahasiswa " + myNim);
+    }
+}
