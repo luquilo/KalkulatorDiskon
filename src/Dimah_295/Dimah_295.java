a1878f71594fbfbb67d699f7c835c724586c60f6
dimdimah <dimahluthfi15@gmail.com>
2023-06-06 23:17:13 +0700

    2113010295

--- src/Dimah_295/Dimah_295.java
+++ src/Dimah_295/Dimah_295.java
@@ -2,8 +2,8 @@ package Dimah_295;
 
 public class Dimah_295 {
     public static void main(String[] args) {
-        int myNim = 2113010295;
         String myName = "Dimah Luthfi Aulia Rohman";
+        int myNim = 2113010295;
         System.out.println("Hallo nama saya " + myName + " dengan Nomer Induk Mahasiswa " + myNim);
     }
 }
