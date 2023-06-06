e0f78b8affbcee119d7b3931d7ba0d554ac3c4e0
HadyanSyaputra <hadyansyaputra00@gmail.com>
2023-06-06 23:40:33 +0700

    2113010296

---
+++ src/Putra_296/Putra_296.java
@@ -0,0 +1,9 @@
+package Putra_296;
+
+public class Putra_296 {
+    public static void main(String[] args) {
+        String jeneng = "Hadyan Syaputra";
+        int nim = 2113010296;
+        System.out.println("Hallo Ich heisse " + jeneng + " mit Studierenden-Identifikationsnummer " + nim);
+    }
+}
