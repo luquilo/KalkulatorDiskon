e4cde5158029974eb0a54da58d1d69dc94fc445d
luquilo <luquilo.etc@gmail.com>
2023-06-06 23:21:58 +0700

    2113010276

---
+++ src/Luqman_276/Luqman_276.java
@@ -0,0 +1,14 @@
+package Luqman_276;
+
+public class Luqman_276 {
+    public static void main(String[] args) {
+        String nama = "muhammad luqmanul hakim";
+        int nim = 2113010276;
+
+        // new commit
+        String pesan = String.format("\nhello world!, my name is %s, and my student id number is %d, glad to see you!!",
+                nama,
+                nim);
+        System.out.println(pesan);
+    }
+}
