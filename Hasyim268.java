1e8a6e1192db99dac5b0206a556909a5e8bba227
Muhammad Hasyim Abdullah <133626937+muhammadhasyimm@users.noreply.github.com>
2023-06-07 12:08:18 +0700

    Add files via upload

---
+++ Hasyim268.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Hasyim268 {
+    public static void main(String[] args) {
+        System.out.println("Nama        : Muhammad Hasyim Abdullah");
+        System.out.println("NIM         : 2113010268");
+        System.out.println("Kelas       : 4G");
+    }
+    
+}
