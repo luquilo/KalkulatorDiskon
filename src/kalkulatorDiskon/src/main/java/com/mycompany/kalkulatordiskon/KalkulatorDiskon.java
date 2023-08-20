a55fca5e065725c1aeefd47c41f884924a2760aa
luquilo <luquilo.etc@gmail.com>
2023-08-20 21:52:48 +0700

    feature: kalkulator GUI version

---
+++ src/kalkulatorDiskon/src/main/java/com/mycompany/kalkulatordiskon/KalkulatorDiskon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kalkulatordiskon;
+
+/**
+ *
+ * @author luqui
+ */
+public class KalkulatorDiskon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
